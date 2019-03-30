d92ddca30fe53983ba83d7e847545a6587f79edc
Nicolas Caballero <thehardyboiz1@gmail.com>
2019-03-30 18:58:57 -0500

    Web page finish

--- js/drag.js
+++ js/drag.js
@@ -1,29 +1,42 @@
 setInterval(() => {
   $('.draggable').draggable();
   console.log('Cambio hecho');
-}, 5000);
+}, 1000);
+
+const sound_good = new Audio();
+const sound_bad = new Audio();
 
 let counter_a = 0;
 let counter_b = 0;
 let counter_c = 0;
 let counter_d = 0;
 
+const playSoundGood = () => {
+  sound_good.src = './sound/good.mp3';
+  sound_good.play();
+};
+const playSoundBad = () => {
+  sound_bad.src = './sound/bad.mp3';
+  sound_bad.play();
+};
+
 $('.sector_a').droppable({
   accept: '.draggable',
-  hoverClass: 'hoversector',
-  drop: function() {
+  hoverClass: 'hoversector_a',
+  drop: function(ev, ui) {
     $('.contador_a').html((counter_a = counter_a + 1));
+    playSoundBad();
   },
   out: function() {
     $('.contador_a').html((counter_a = counter_a - 1));
-  },
-  tolerance: 'pointer'
+  }
 });
 $('.sector_b').droppable({
   accept: '.draggable',
-  hoverClass: 'hoversector',
+  hoverClass: 'hoversector_b',
   drop: function() {
     $('.contador_b').html((counter_b = counter_b + 1));
+    playSoundGood();
   },
   out: function() {
     $('.contador_b').html((counter_b = counter_b - 1));
@@ -31,9 +44,10 @@ $('.sector_b').droppable({
 });
 $('.sector_c').droppable({
   accept: '.draggable',
-  hoverClass: 'hoversector',
+  hoverClass: 'hoversector_c',
   drop: function() {
     $('.contador_c').html((counter_c = counter_c + 1));
+    playSoundBad();
   },
   out: function() {
     $('.contador_c').html((counter_c = counter_c - 1));
@@ -41,9 +55,10 @@ $('.sector_c').droppable({
 });
 $('.sector_d').droppable({
   accept: '.draggable',
-  hoverClass: 'hoversector',
+  hoverClass: 'hoversector_d',
   drop: function() {
     $('.contador_d').html((counter_d = counter_d + 1));
+    playSoundBad();
   },
   out: function() {
     $('.contador_d').html((counter_d = counter_d - 1));
